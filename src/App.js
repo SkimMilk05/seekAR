e80de0c7954273170ab11d7ff6218c6129316bd3
Sion Kim <sk9uth@virginia.edu>
2021-03-28 11:02:20 -0400

    build errors

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = { //fields
-            start_game: true,
             hide: false,
             link: false,
 
